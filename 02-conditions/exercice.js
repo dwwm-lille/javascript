2f01bb488e54e609db16c3840517a2d0ce6eced5
Matthieu Mota <matthieumota@gmail.com>
2022-12-06 10:50:24 +0100

    Conditions

--- 02-conditions/exercice.js
+++ 02-conditions/exercice.js
@@ -19,14 +19,42 @@ var frigo = {
     fromage: 1,
 };
 
-console.log(frigo);
+function verify() {
+    console.log(frigo);
 
-if (frigo.pain >= 1 && frigo.jambon >= 1 && (frigo.tomate >= 1 || frigo.fromage >= 1)) {
-    alert('Je peux faire un sandwich');
-} else if (frigo.poulet >= 1) {
-    alert('Je mange le poulet');
-} else if (frigo.oeuf >= 3) {
-    alert('Je mange une omelette');
-} else {
-    alert('Je mange des chips');
+    if (frigo.pain >= 1 && frigo.jambon >= 1 && (frigo.tomate >= 1 || frigo.fromage >= 1)) {
+        frigo.pain--; // frigo.pain = frigo.pain - 1;
+        frigo.jambon--;
+        if (frigo.tomate >= 1) {
+            rest = frigo.tomate--;
+        } else {
+            rest = frigo.fromage--;
+        }
+
+        // Le ternaire... ? = if, : = else
+        // rest = (frigo.tomate >= 1) ? frigo.tomate-- : frigo.fromage--;
+        // (frigo.tomate >= 1) ? frigo.tomate-- : ((frigo.machin) ? '...' : '...');
+
+        alert('Je peux faire un sandwich');
+    } else if (frigo.poulet >= 1) {
+        frigo.poulet--;
+        alert('Je mange le poulet');
+    } else if (frigo.oeuf >= 3) {
+        frigo.oeuf -= 3;
+        alert('Je mange une omelette');
+    } else {
+        alert('Je mange des chips');
+    }
+}
+
+function fill() {
+    frigo = {
+        tomate: parseInt(prompt('Combien de tomate ?')),
+        oeuf: parseInt(prompt('Combien d\'oeuf ?')),
+        pain: parseInt(prompt('Combien de pain ?')),
+        jambon: parseInt(prompt('Combien de jambon ?')),
+        poulet: parseInt(prompt('Combien de poulet ?')),
+        // parseInt va transformer une chaine en nombre
+        fromage: parseInt(prompt('Combien de fromage ?')),
+    };
 }
