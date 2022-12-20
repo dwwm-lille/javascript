5cf1e077e0f14a864cb4fdd0da567496eac47405
Matthieu Mota <matthieumota@gmail.com>
2022-12-20 20:34:56 +0100

    Array multi

--- 10-revisions/array.js
+++ 10-revisions/array.js
@@ -77,3 +77,65 @@ for (let country of countries) {
     img.src = 'https://raw.githubusercontent.com/stevenrskelton/flag-icon/master/png/75/country-4x3/'+iso+'.png';
     li.appendChild(img);
 }
+
+// Tableaux multidimensionnels
+let car = ['Renault', 'Mégane']; // 1 dimension
+car.push([]);
+
+let garage = [ // 2 dimensions
+    car,
+    ['Alfa Roméo', 'Giulia', []],
+    ['Peugeot', '308', ['Sac', 10, 'Roue']] // 3 dimensions
+];
+
+// Comment avoir le modèle de la 2ème voiture (Alfa Roméo) ?
+console.log(garage[1][1]); // Giulia
+
+// Comment chercher le sac ?
+console.log( garage[2][2][0] );
+
+// En modifiant la variable car, on modifie aussi le garage
+car[2].push('Valise');
+
+// On veut afficher Marque et modèle dans une liste
+const ulCar = document.createElement('ul');
+document.body.appendChild(ulCar);
+
+// car est un tableau
+for (let car of garage) {
+    console.log(car);
+    const liCar = document.createElement('li');
+    liCar.innerHTML = car[0] + ' ' + car[1];
+
+    // Comment afficher le coffre s'il n'est pas vide ?
+    // Le ?. permet de s'assurer que l'index 2 existe bien dans le tableau
+    if (car[2]?.length > 0) {
+        liCar.innerHTML += '. Dans le coffre, il y a ' + car[2];
+
+        // On va créer une liste dans la liste pour afficher le contenu du coffre
+        const ulTrunk = document.createElement('ul');
+        for (let item of car[2]) {
+            const liTrunk = document.createElement('li');
+            liTrunk.innerHTML = item;
+            ulTrunk.appendChild(liTrunk);
+        }
+        liCar.appendChild(ulTrunk);
+    }
+
+    ulCar.appendChild(liCar);
+}
+
+// Tableau d'objets
+let garage2 = [
+    { brand: 'Renault', model: 'Mégane', trunk: [1, 2] },
+    { brand: 'Alfa Roméo', model: 'Giulia', trunk: [] },
+];
+
+for (let car of garage2) {
+    console.log(car.brand + ' ' + car.model);
+
+    // Parcourir le coffre
+    for (let item of car.trunk) {
+        console.log(item);
+    }
+}
